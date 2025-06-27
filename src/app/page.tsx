299e5e7148f14a005265ba6244f4895b50e736f9
Aashish <aashishdubey.dev@gmail.com>
2025-06-27 09:55:43 +0530

    Fix: added some drizzle scripts to package.json file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
-import { Button } from "@/components/ui/button";
+
 
 export default function Home() {
   return (
-    <div >
-      <h1 className="text-4xl font-bold text-amber-300">Hello from Next</h1>
-      <Button variant='ghost'>Click Me</Button>
+    <div className="bg-black h-screen">
+      <div className="bg-black px-5 py-2">
+        <div className='relative h-16 w-16 m-5'>
+          <div className="absolute top-0 bottom-3 bg-gradient-to-tr from-amber-50 to-amber-100 h-full w-full blur-md"></div>
+          <div className="bg-black h-full rounded-lg relative border-1 border-white/50 hover:border-white/70"></div>
+      </div>
+      </div>
+
     </div>
   )  
 }
