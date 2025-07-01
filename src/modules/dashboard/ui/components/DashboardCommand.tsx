fd98cb0ebe818d807206302bf10bc63e58e97497
Aashish <aashishdubey.dev@gmail.com>
2025-07-01 10:13:56 +0530

    Added dashboard Navbar

---
+++ src/modules/dashboard/ui/components/DashboardCommand.tsx
@@ -0,0 +1,27 @@
+import { CommandDialog, CommandInput, CommandList } from "@/components/ui/command"
+import { CommandItem } from "cmdk";
+
+import { Dispatch,SetStateAction } from "react";
+
+interface Props {
+    open :boolean,
+    setOpen : Dispatch<SetStateAction<boolean>>;
+}
+
+
+const DashboardCommand = ({open,setOpen}:Props)=>{
+    return (
+        <CommandDialog open={open} onOpenChange={setOpen}>
+            <CommandInput 
+                placeholder="Find a meeting or agent"
+            />
+            <CommandList>
+                <CommandItem>
+                    Test
+                </CommandItem>
+            </CommandList>
+        </CommandDialog>
+    )
+}
+
+export default DashboardCommand
--- src/modules/dashboard/ui/components/DashboardUserButton.tsx
+++ src/modules/dashboard/ui/components/DashboardUserButton.tsx
@@ -10,15 +10,29 @@ import {
     DropdownMenuSeparator,
     DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu'
+
+import {
+    Drawer,
+    DrawerContent,
+    DrawerDescription,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerTitle,
+    DrawerTrigger
+} from '@/components/ui/drawer'
+
 import { Avatar } from "@/components/ui/avatar"
 import { AvatarImage } from "@radix-ui/react-avatar"
 import GeneratedAvatar from "@/components/generated-avatar"
 import { ChevronDownCircleIcon, ChevronDownIcon, CreditCardIcon, LogOutIcon } from "lucide-react"
 import { useRouter } from "next/navigation"
+import { useIsMobile } from "@/hooks/use-mobile"
+import { Button } from "@/components/ui/button"
 
 const DashboardUserButton = () => {
 
-    const router = useRouter()
+    const router = useRouter();
+    const isMobile = useIsMobile()
 
     const {data,isPending} = authClient.useSession()
 
@@ -33,6 +47,50 @@ const DashboardUserButton = () => {
     if(isPending || !data?.user) {
         return null
     }
+
+    if(isMobile){
+        return(
+            <Drawer >
+                <DrawerTrigger className="rounded-lg border border-border/10 p-3 w-full flex items-center justify-center bg-[#222831]/5 hover:bg-[#222831]/10 overflow-hidden gap-2.5">
+                {data.user.image?(
+                <Avatar>
+                    <AvatarImage src={data.user.image}/>  
+                </Avatar>
+            ): (
+                <GeneratedAvatar seed={data.user.name} variant="intials" classname="size-9 mr-3"/>
+            ) }
+            <div className="flex flex-col gap-0.5 text-left overflow-hidden flex-1 min-w-0">
+                <p className="text-sm truncate w-full">{data.user.name}</p>
+                <p className="text-xs truncate w-full">{data.user.email}</p>
+            </div>
+            <ChevronDownIcon className="size-4 shrink-0"/>
+                </DrawerTrigger>
+                <DrawerContent>
+                    <DrawerHeader>
+                        <DrawerTitle>{data.user.name}</DrawerTitle>
+                        <DrawerDescription>{data.user.email}</DrawerDescription>
+                    </DrawerHeader>
+                    <DrawerFooter>
+                        <Button
+                            variant="outline"
+                            onClick={()=>{}}
+                        >
+                            <CreditCardIcon className="size-4 text-black"/>
+                            Billing
+                        </Button>
+                        <Button
+                            variant="outline"
+                            onClick={()=>{}}
+                        >
+                            <LogOutIcon className="size-4 text-black"/>
+                            Logout
+                        </Button>
+                    </DrawerFooter>
+                </DrawerContent>
+            </Drawer>
+        )
+    }
+
 
   return (
     <DropdownMenu>
