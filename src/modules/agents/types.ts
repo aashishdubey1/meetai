793afa71e5bb0b7ea0176a222654dde1ea4b4f9f
Aashish <aashishdubey.dev@gmail.com>
2025-07-04 12:18:06 +0530

    Added meeting-data-table

--- src/modules/agents/types.ts
+++ src/modules/agents/types.ts
@@ -5,3 +5,4 @@ import { inferRouterOutputs } from '@trpc/server'
 import type {AppRouter} from "@/trpc/routers/_app"
 
 export type AgentGetOne = inferRouterOutputs<AppRouter>["agents"]["getOne"]
+export type AgentGetMany = inferRouterOutputs<AppRouter>["agents"]["getMany"]['items']
